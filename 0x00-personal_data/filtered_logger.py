091013789fba06b6b4e17f48947f5f0faddafae4
Zaim211 <yousseftitazaim@gmail.com>
2024-04-11 23:10:57 +0000

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -64,14 +64,14 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Function that returns a connector to the database """
     user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
-    pssd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
     host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    con = mysql.connector.connect(user=user,
-                                  password=pssd,
-                                  host=host,
-                                  db_name=db_name)
-    return con
+    conn = mysql.connector.connect(user=user,
+                                   password=passwd,
+                                   host=host,
+                                   database=db_name)
+    return conn
 
 
 def main():

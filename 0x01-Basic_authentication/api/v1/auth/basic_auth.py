7c4942e616ec4de1d6ab1e3cc1ed250d590ce79f
Zaim211 <yousseftitazaim@gmail.com>
2024-04-15 21:06:54 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic auth """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
             return None
         basic_auth = authorization_header.split(" ")[-1]
         return basic_auth
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ Method that returns the decoded value of a
+        Base64 string base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64_authorization_header.encode('utf-8')
+            decode64 = base64.b64decode(decoded)
+            return decode64.decode('utf-8')
+        except Exception:
+            return None

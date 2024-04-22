d4ab7d6c3fdb4c9d64de218f19d7e1b6612a3664
Zaim211 <yousseftitazaim@gmail.com>
2024-04-22 22:09:35 +0000

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Flask app """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -35,6 +35,16 @@ def login():
     response = jsonify({"email": f"{email}", "message": "logged in"})
     response.set_cookie("session_id", new_session_id)
     return response
+
+@app.route('sessions', methods=["DELETE"], strict_slashes=False)
+def logout():
+    """ Logout the users """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

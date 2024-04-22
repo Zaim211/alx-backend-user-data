e0b35eb0aa57143a77d654ce259b4cd66f7367fc
Zaim211 <yousseftitazaim@gmail.com>
2024-04-22 21:45:14 +0000

    Log In

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Flask app """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -8,10 +8,12 @@ AUTH = Auth()
 
 @app.route('/', methods=["GET"], strict_slashes=False)
 def main() -> str:
+    """ Test the route """
     return jsonify({"message": "Bienvenue"})
 
 @app.route('/users', methods=["POST"], strict_slashes=False) 
 def users():
+    """ new users registration """
     email = request.form.get("email")
     password = request.form.get("password")
 
@@ -20,6 +22,19 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f"{email}", "message": "user created"})
+
+@app.route('/sessions', methods=["POST"], strict_slashes=False)
+def login():
+    """ login the users """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    new_session_id = AUTH.create_session(email)
+    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response.set_cookie("session_id", new_session_id)
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

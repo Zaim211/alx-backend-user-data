d4fd6ae939ab401fa936b360a8d1b21da6846d05
Zaim211 <yousseftitazaim@gmail.com>
2024-04-23 19:26:05 +0000

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,7 +10,7 @@ AUTH = Auth()
 
 
 @app.route('/', methods=["GET"], strict_slashes=False)
-def main() -> str:
+def index():
     """ Test the route """
     return jsonify({"message": "Bienvenue"})
 
